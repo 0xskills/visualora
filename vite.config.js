a2a06cb050369fa073d4b6c5bf4ab3390a9f026b
VDamas <vitoriodamas@gmail.com>
2025-02-28 20:32:11 +0000

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from "vite"
 export default defineConfig({
-    base: "/visualora/"
+	base: '/visualora',
+	/*
+    server: {
+        https: {
+            cert: './ssl/certificate.crt',
+            key: './ssl/private.key'
+        }
+    }
+	*/
 })
 
 
